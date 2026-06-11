2e423b9cd233fb0b772f4d2fa2ba38e674469a21
Hariharan Balaji <hbalaji@eagleconsulting.co.in>
2026-06-11 15:53:25 +0530

    UI enhancement and deployment metadata

---
+++ README.md
@@ -0,0 +1,23 @@
+Developer
+    ↓
+GitHub Repository
+    ↓
+GitHub Actions
+    ↓
+Unit Test (Pytest)
+    ↓
+SonarQube Scan
+    ↓
+Docker Build
+    ↓
+Trivy Scan
+    ↓
+Push to AWS ECR
+    ↓
+Helm Deploy
+    ↓
+Amazon EKS
+    ↓
+AWS Load Balancer
+    ↓
+Flask Application
--- app/app.py
+++ app/app.py
@@ -1,25 +1,41 @@
-from flask import Flask, render_template
-import socket
-import datetime
+from flask import Flask, render_template, jsonify
 import os
+import socket
 
 app = Flask(__name__)
+
+APP_VERSION = os.getenv("APP_VERSION", "dev")[:7]
+ENVIRONMENT = os.getenv("ENVIRONMENT", "Practice")
+DEPLOY_TIME = os.getenv("DEPLOY_TIME", "Not Available")
+POD_NAME = socket.gethostname()
 
 @app.route("/")
 def home():
     return render_template(
         "index.html",
-        hostname=socket.gethostname(),
-        version=os.getenv("APP_VERSION", "1.0.0"),
-        environment=os.getenv("ENVIRONMENT", "Production"),
-        current_time=str(datetime.datetime.now())
+        version=APP_VERSION,
+        environment=ENVIRONMENT,
+        deploy_time=DEPLOY_TIME,
+        pod_name=POD_NAME
     )
 
 @app.route("/health")
 def health():
-    return {
-        "status": "healthy"
-    }, 200
+    return jsonify(
+        {
+            "status": "UP",
+            "application": "enterprise-cicd-pipeline"
+        }
+    )
+
+@app.route("/version")
+def version():
+    return jsonify(
+        {
+            "version": APP_VERSION,
+            "environment": ENVIRONMENT
+        }
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
